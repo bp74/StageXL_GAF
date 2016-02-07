f913c0b34a9fe02f16de23c97a53bcf458e1b58b
Bernhard <bernhard.robert.pichler@gmail.com>
2016-02-07 10:02:57 +0100

    More precise generic type

--- lib/src/display/gaf_movie_clip.dart
+++ lib/src/display/gaf_movie_clip.dart
@@ -23,7 +23,7 @@ class GAFMovieClip extends DisplayObjectContainer implements GAFDisplayObject, A
 
   final GAFTimeline _timeline;
 
-  final Map<String, DisplayObject> _displayObjects = new Map<String, DisplayObject>();
+  final Map<String, GAFDisplayObject> _displayObjects = new Map<String, GAFDisplayObject>();
   final List<GAFMovieClip> _movieClips = new List<GAFMovieClip>();
 
   Point<num> maxSize = null;
